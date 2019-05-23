28a7fbcee488bc906332ae8d7516069bb89d4195
julioantonini <julioantonini@gmail.com>
2019-05-23 20:20:24 -0300

    build

---
+++ build/precache-manifest.c952e57c8daaad4e5de4708745e038fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb004a4b133a2cd99d188925b0fd87c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "269875163bba7c6b3961",
+    "url": "/static/css/main.84668902.chunk.css"
+  },
+  {
+    "revision": "8be471476e33c4f21dc2",
+    "url": "/static/js/2.cbe6f518.chunk.js"
+  },
+  {
+    "revision": "269875163bba7c6b3961",
+    "url": "/static/js/main.742bcba5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
